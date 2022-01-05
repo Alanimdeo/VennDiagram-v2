50f9c641e937b0803099fe52ca8c9997c0123bcd
Alanimdeo <alan@imdeo.kr>
2022-01-06 03:50:27 +0900

    /목록 명령어의 출력이 Embed를 이용하도록 수정

--- dist/commands/queue.js
+++ dist/commands/queue.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const builders_1 = require("@discordjs/builders");
+const discord_js_1 = require("discord.js");
 const types_1 = require("../types");
 module.exports = new types_1.Command(new builders_1.SlashCommandBuilder().setName("목록").setDescription("재생 목록을 확인합니다."), async (interaction, bot) => {
-    let message = `:scroll: ${interaction.guild?.name} 서버의 재생 목록`;
+    await interaction.deferReply();
+    let message = "";
     let guildQueue = bot.player.queue.get(interaction.guildId ? interaction.guildId : "");
     if (guildQueue) {
         let length = String(guildQueue.songs.length).length;
@@ -12,6 +14,14 @@ module.exports = new types_1.Command(new builders_1.SlashCommandBuilder().setNam
         });
     }
     else
-        message += "이 없습니다.";
-    await interaction.reply(message);
+        message += "..노래가 없어요. `/재생 (노래 제목 또는 유튜브 링크)` 명령어를 통해 노래를 틀어 보세요!";
+    await interaction.editReply({
+        embeds: [
+            new discord_js_1.MessageEmbed({
+                color: "#0067a3",
+                title: `:scroll: ${interaction.guild?.name} 서버의 재생 목록`,
+                description: message.slice(2),
+            }),
+        ],
+    });
 });
--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -1,18 +1,27 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, MessageEmbed } from "discord.js";
 import { Bot, Command, Song } from "../types";
 
 module.exports = new Command(
     new SlashCommandBuilder().setName("목록").setDescription("재생 목록을 확인합니다."),
     async (interaction: CommandInteraction, bot: Bot) => {
-        let message = `:scroll: ${interaction.guild?.name} 서버의 재생 목록`;
+        await interaction.deferReply();
+        let message = "";
         let guildQueue = bot.player.queue.get(interaction.guildId ? interaction.guildId : "");
         if (guildQueue) {
             let length = String(guildQueue.songs.length).length;
             guildQueue.songs.forEach((song: Song, index: number) => {
                 message += `\n\`${String(index + 1).padStart(length, "0")}. ${song.title} (${song.duration})\``;
             });
-        } else message += "이 없습니다.";
-        await interaction.reply(message);
+        } else message += "..노래가 없어요. `/재생 (노래 제목 또는 유튜브 링크)` 명령어를 통해 노래를 틀어 보세요!";
+        await interaction.editReply({
+            embeds: [
+                new MessageEmbed({
+                    color: "#0067a3",
+                    title: `:scroll: ${interaction.guild?.name} 서버의 재생 목록`,
+                    description: message.slice(2),
+                }),
+            ],
+        });
     }
 );
