d60c51498b15fffd8b6f6c4cb0b946745b88f69b
Alanimdeo <alan@imdeo.kr>
2022-01-06 03:45:23 +0900

    목록 명령어 사용 시 메시지 소폭 변경, 목록 없을 때 메시지 추가

--- dist/commands/queue.js
+++ dist/commands/queue.js
@@ -3,13 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const builders_1 = require("@discordjs/builders");
 const types_1 = require("../types");
 module.exports = new types_1.Command(new builders_1.SlashCommandBuilder().setName("목록").setDescription("재생 목록을 확인합니다."), async (interaction, bot) => {
-    let message = "";
+    let message = `:scroll: ${interaction.guild?.name} 서버의 재생 목록`;
     let guildQueue = bot.player.queue.get(interaction.guildId ? interaction.guildId : "");
     if (guildQueue) {
         let length = String(guildQueue.songs.length).length;
-        guildQueue.songs.forEach(async (song, index) => {
+        guildQueue.songs.forEach((song, index) => {
             message += `\n\`${String(index + 1).padStart(length, "0")}. ${song.title} (${song.duration})\``;
         });
     }
-    await interaction.reply(`:scroll: ${interaction.guild?.name} 서버의 재생 목록${message}`);
+    else
+        message += "이 없습니다.";
+    await interaction.reply(message);
 });
--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -5,14 +5,14 @@ import { Bot, Command, Song } from "../types";
 module.exports = new Command(
     new SlashCommandBuilder().setName("목록").setDescription("재생 목록을 확인합니다."),
     async (interaction: CommandInteraction, bot: Bot) => {
-        let message = "";
+        let message = `:scroll: ${interaction.guild?.name} 서버의 재생 목록`;
         let guildQueue = bot.player.queue.get(interaction.guildId ? interaction.guildId : "");
         if (guildQueue) {
             let length = String(guildQueue.songs.length).length;
-            guildQueue.songs.forEach(async (song: Song, index: number) => {
+            guildQueue.songs.forEach((song: Song, index: number) => {
                 message += `\n\`${String(index + 1).padStart(length, "0")}. ${song.title} (${song.duration})\``;
             });
-        }
-        await interaction.reply(`:scroll: ${interaction.guild?.name} 서버의 재생 목록${message}`);
+        } else message += "이 없습니다.";
+        await interaction.reply(message);
     }
 );
