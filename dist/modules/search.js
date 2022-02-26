ea1a64f40bd8da03b40b701e42b10f1b02bd7ae0
Alanimdeo <alan@imdeo.kr>
2022-02-27 03:26:44 +0900

    v2.0.3 - 검색 알고리즘 수정(뮤직비디오와 가사 영상이 우선 배치되고 라이브/직캠 영상이 나중에 배치됨)

--- dist/modules/search.js
+++ dist/modules/search.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.search = void 0;
 const ytsr_1 = __importDefault(require("ytsr"));
-async function search(keyword, limit) {
+const musicFilter = ["[mv]", "mv", "music video", "가사", "lyrics"];
+async function search(keyword, limit = 5) {
     return new Promise(async (resolve, reject) => {
         const filters = await ytsr_1.default.getFilters(keyword);
         const typeFilters = filters.get("Type");
@@ -14,8 +15,24 @@ async function search(keyword, limit) {
         const filter = typeFilters.get("Video");
         if (!filter || !filter.url)
             return reject(new Error("resultNotFound"));
-        const searchResult = await (0, ytsr_1.default)(filter.url, { hl: "ko", gl: "KR", limit: limit ? limit : 5 });
-        return resolve(searchResult.items);
+        let searchResult = (await (0, ytsr_1.default)(filter.url, { hl: "ko", gl: "KR", limit: limit > 10 ? limit : 10 })).items;
+        if (!searchResult || searchResult.length === 0)
+            return reject(new Error("resultNotFound"));
+        searchResult.sort((a, b) => {
+            if (a.type !== "video" || b.type !== "video")
+                return 0;
+            const lowerA = a.title.toLowerCase();
+            const lowerB = b.title.toLowerCase();
+            const filterA = musicFilter.some((f) => lowerA.includes(f));
+            const filterB = musicFilter.some((f) => lowerB.includes(f));
+            if (filterA && !filterB)
+                return -1;
+            else if (filterA && filterB)
+                return 0;
+            else
+                return 1;
+        });
+        return resolve(searchResult.slice(0, limit));
     });
 }
 exports.search = search;
--- src/modules/search.ts
+++ src/modules/search.ts
@@ -1,13 +1,26 @@
 import ytsr from "ytsr";
 
-export async function search(keyword: string, limit?: number): Promise<ytsr.Item[]> {
+const musicFilter = ["[mv]", "mv", "music video", "가사", "lyrics"];
+
+export async function search(keyword: string, limit: number = 5): Promise<ytsr.Item[]> {
     return new Promise(async (resolve, reject) => {
         const filters = await ytsr.getFilters(keyword);
         const typeFilters = filters.get("Type");
         if (!typeFilters) return reject(new Error("resultNotFound"));
         const filter = typeFilters.get("Video");
         if (!filter || !filter.url) return reject(new Error("resultNotFound"));
-        const searchResult = await ytsr(filter.url, { hl: "ko", gl: "KR", limit: limit ? limit : 5 });
-        return resolve(searchResult.items);
+        let searchResult = (await ytsr(filter.url, { hl: "ko", gl: "KR", limit: limit > 10 ? limit : 10 })).items;
+        if (!searchResult || searchResult.length === 0) return reject(new Error("resultNotFound"));
+        searchResult.sort((a, b) => {
+            if (a.type !== "video" || b.type !== "video") return 0;
+            const lowerA = a.title.toLowerCase();
+            const lowerB = b.title.toLowerCase();
+            const filterA = musicFilter.some((f) => lowerA.includes(f));
+            const filterB = musicFilter.some((f) => lowerB.includes(f));
+            if (filterA && !filterB) return -1;
+            else if (filterA && filterB) return 0;
+            else return 1;
+        });
+        return resolve(searchResult.slice(0, limit));
     });
 }
