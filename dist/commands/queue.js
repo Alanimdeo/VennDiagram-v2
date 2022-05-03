d7bd5f7dff91edbe7e96ef054826f349d6fbaf6b
alanimdeo <alan@imdeo.kr>
2022-05-04 01:02:26 +0900

    v2.4.0 - /목록 명령어 사용 시 현재 반복 모드와 재생/일시정지 상태 표시 기능 추가

--- dist/commands/queue.js
+++ dist/commands/queue.js
@@ -10,16 +10,21 @@ module.exports = new types_1.Command(new builders_1.SlashCommandBuilder().setNam
     if (guildQueue && guildQueue.songs.length !== 0) {
         let length = String(guildQueue.songs.length).length;
         guildQueue.songs.forEach((song, index) => {
-            message += `\n\`${String(index + 1).padStart(length, "0")}. ${song.title} (${song.duration})\``;
+            message += `\n${index === 0 ? (guildQueue?.isPlaying ? ":arrow_forward:" : ":pause_button:") : ""}\`${String(index + 1).padStart(length, "0")}. ${song.title} (${song.duration})\``;
         });
     }
     else
         message += ".노래가 없어요. `/재생 (노래 제목 또는 유튜브 링크)` 명령어를 통해 노래를 틀어 보세요!";
+    const repeatMode = guildQueue
+        ? " (반복 모드: " +
+            (guildQueue.repeatMode === "none" ? "없음" : guildQueue.repeatMode === "song" ? "곡" : "전체") +
+            ")"
+        : "";
     await interaction.editReply({
         embeds: [
             new discord_js_1.MessageEmbed({
                 color: "#0067a3",
-                title: `:scroll: ${interaction.guild?.name} 서버의 재생 목록`,
+                title: `:scroll: ${interaction.guild?.name}의 재생 목록${repeatMode}`,
                 description: message.slice(1),
             }),
         ],
