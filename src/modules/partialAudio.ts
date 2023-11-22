a27fea53c78817f676d284c64cc4960f7d558753
Alanimdeo <alan@imdeo.kr>
2023-11-22 09:18:34 +0900

    Enhance partialAudio

--- src/modules/partialAudio.ts
+++ src/modules/partialAudio.ts
@@ -2,13 +2,21 @@ import stream from "stream";
 import ffmpeg from "fluent-ffmpeg";
 import ytdl from "ytdl-core";
 
-export const getVideoInfo = async (videoUrl: string) =>
-  (await ytdl.getInfo(videoUrl)).formats.filter((format) => format.mimeType?.startsWith("audio/mp4"))[0];
+export const getVideoInfo = async (videoUrl: string) => {
+  const video = await ytdl.getInfo(videoUrl);
+  const formats = video.formats.filter((format) => format.hasAudio === true && format.audioCodec === "opus");
+  formats.sort((a, b) => {
+    if (a.hasVideo && !b.hasVideo) return 1;
+    if (!a.hasVideo && b.hasVideo) return -1;
+    return 0;
+  });
+  return formats[0];
+};
 
 export async function getPartialAudio(videoUrl: string, startFrom: number) {
   const videoInfo = await getVideoInfo(videoUrl);
 
-  const ff = ffmpeg(videoInfo.url).seek(startFrom).audioCodec("opus").format("ogg");
+  const ff = ffmpeg(videoInfo.url).seek(startFrom).format("ogg");
 
   const ffStream = new stream.PassThrough();
   ff.pipe(ffStream, { end: true });
